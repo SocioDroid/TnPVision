ed951390c21fa63bad03cd93b274911cae39ce45
leet <sociodroid2019@gmail.com>
2021-02-23 13:01:06 +0530

    Added Review Component

--- src/views/reports/InterviewerDashboardView/StudentInformation.js
+++ src/views/reports/InterviewerDashboardView/StudentInformation.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, Typography, Divider, TextField } from '@material-ui/core';
-import Rating from '@material-ui/lab/Rating';
+
 import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -14,53 +13,75 @@ import Grid from '@material-ui/core/Grid';
 import { useParams } from 'react-router-dom';
 import StudentService from '../../../services/studentService';
 import { Fullscreen } from '@material-ui/icons';
+import ReviewTabs from './WriteReview';
+import RoundsService from '../../../services/RoundService';
+import InterviewerService from '../../../services/InterviewerService';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
     minHeight: 190,
-    width: "100px"
+    width: '100px'
   },
   paper2: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    minHeight: 620,
-  },
+    minHeight: 620
+  }
 }));
 
 export default function CenteredGrid() {
   const classes = useStyles();
   const { id } = useParams();
   const [student, setStudent] = useState(null);
-  const [technical, setTechnical] = React.useState(0);
-  const [communication, setCommunication] = React.useState(0);
-  const [personality, setPersonality] = React.useState(0);
+
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(2);
-
+  const [rounds, setRounds] = React.useState(null);
+  const [drive, setDrive] = React.useState(null);
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event) => {
+  const handleChangeRowsPerPage = event => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
 
   useEffect(() => {
-    StudentService.getSingleStudentwithid(id)
+    StudentService.getSingleStudentwithid(id).then(res => {
+      console.log('Interviewer Student ', res.data);
+      setStudent(res.data);
+    });
+
+    InterviewerService.getDrive()
       .then(res => {
-        console.log("Interviewer Student ", res.data);
-        setStudent(res.data);
-        //console.log("Student : ", student);
+        console.log('Interviewer Drive--- ', res.data);
+        setDrive(res.data);
       })
+      .catch(error => {
+        console.log(error);
+      });
   }, []);
+
+  useEffect(() => {
+    if (drive) {
+      RoundsService.getAllRounds(drive.id)
+        .then(res => {
+          console.log('Drive Rounds--- ', res.data);
+          setRounds(res.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, [drive]);
 
   if (student) {
     return (
@@ -68,133 +89,108 @@ export default function CenteredGrid() {
         <Grid container spacing={3}>
           <Grid item xs={3}>
             <Paper className={classes.paper2}>
-              {id}. {student.user.first_name} {student.user.last_name} <br /><br />
+              {id}. {student.user.first_name} {student.user.last_name} <br />
+              <br />
               {student.user.email}
             </Paper>
           </Grid>
           <Grid item xs={9}>
-
             <Paper className={classes.paper2}>
               <Grid container spacing={3}>
-                  <Grid item xs={12}>
-                    Education <br /><br />
-                    <TableContainer component={Paper}>
-                      <Table>
-                        <TableHead>
-                          <TableRow>
-                            <TableCell align="center">10th</TableCell>
-                            <TableCell align="center">12th</TableCell>
-                            <TableCell align="center">Diploma</TableCell>
-                            <TableCell align="center">Engineering</TableCell>
-                          </TableRow>
-                        </TableHead>
-                        <TableBody>
-                          <TableRow>
-                            <TableCell align="center">{student.tenthPercentage}</TableCell>
-                            <TableCell align="center">{student.twelthPercentage === 0 ? "NA" : student.twelthPercentage}</TableCell>
-                            <TableCell align="center">{student.diplomaPercentage === 0 ? "NA" : student.diplomaPercentage}</TableCell>
-                            <TableCell align="center">{student.enggAggPercentage}</TableCell>
-                          </TableRow>
-                        </TableBody>
-                      </Table>
-                    </TableContainer>
-                  </Grid>
-                  
-                  <Grid item xs={12}>
-                    Certification <br /><br />
-                      <TableContainer component={Paper}>
-                        <Table>
-                          <TableHead>
-                            <TableRow>
-                              <TableCell align="center">Name</TableCell>
-                              <TableCell align="center">Issued Date</TableCell>                      
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            {student.certificates.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((certificate, index) => {
-                              return(
-                                <TableRow key={index}>                              
-                                  <TableCell align="center">{certificate.name}</TableCell>
-                                  <TableCell align="center">{certificate.issuedDate}</TableCell>                                      
-                                </TableRow>
-                              )
-                            })}
-                            
-                          </TableBody>
-                        </Table>
-                      </TableContainer>
-                      <TablePagination
-                        rowsPerPageOptions={[10, 25, 100]}
-                        component="div"
-                        count={student.certificates.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                      />
-                  </Grid>
+                <Grid item xs={12}>
+                  Education <br />
+                  <br />
+                  <TableContainer component={Paper}>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell align="center">10th</TableCell>
+                          <TableCell align="center">12th</TableCell>
+                          <TableCell align="center">Diploma</TableCell>
+                          <TableCell align="center">Engineering</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell align="center">
+                            {student.tenthPercentage}
+                          </TableCell>
+                          <TableCell align="center">
+                            {student.twelthPercentage === 0
+                              ? 'NA'
+                              : student.twelthPercentage}
+                          </TableCell>
+                          <TableCell align="center">
+                            {student.diplomaPercentage === 0
+                              ? 'NA'
+                              : student.diplomaPercentage}
+                          </TableCell>
+                          <TableCell align="center">
+                            {student.enggAggPercentage}
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Grid>
 
-
+                <Grid item xs={12}>
+                  Certification <br />
+                  <br />
+                  <TableContainer component={Paper}>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell align="center">Name</TableCell>
+                          <TableCell align="center">Issued Date</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {student.certificates
+                          .slice(
+                            page * rowsPerPage,
+                            page * rowsPerPage + rowsPerPage
+                          )
+                          .map((certificate, index) => {
+                            return (
+                              <TableRow key={index}>
+                                <TableCell align="center">
+                                  {certificate.name}
+                                </TableCell>
+                                <TableCell align="center">
+                                  {certificate.issuedDate}
+                                </TableCell>
+                              </TableRow>
+                            );
+                          })}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                  <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={student.certificates.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                  />
+                </Grid>
               </Grid>
-          </Paper>
+            </Paper>
           </Grid>
         </Grid>
 
         <div>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-              <Card>
-                <CardContent>
-                  <Typography className={classes.bold} variant="h6">
-                    Technical
-                  </Typography>
-                  <Rating
-                    name="technical"
-                    value={technical}
-                    onChange={(event, newValue) => {
-                      setTechnical(newValue);
-                    }}
-                  />
-                  <Typography className={classes.bold} variant="h6">
-                    Communication
-                      </Typography>
-                  <Rating
-                    name="communication"
-                    value={communication}
-                    onChange={(event, newValue) => {
-                      setCommunication(newValue);
-                    }}
-                  />
-                  <Typography className={classes.bold} variant="h6">
-                    Personality
-                  </Typography>
-                  <Rating
-                    name="personality"
-                    value={personality}
-                    onChange={(event, newValue) => {
-                      setPersonality(newValue);
-                    }}
-                  />
-                  <br />
-                  <TextField
-                    id="comment"
-                    label="Write a Review"
-                    multiline
-                    rows={4}
-                    variant="outlined"
-                  />
-                </CardContent>
-              </Card>
+              {rounds && <ReviewTabs rounds={rounds} drive_id={drive.id} student_id={student.id}/>}
             </Grid>
           </Grid>
         </div>
       </div>
-
     );
-  }
-  else {
-    return (
-      <div>{student}</div>
-    );
+  } else {
+    return <div>{student}</div>;
   }
 }
--- src/views/reports/InterviewerDashboardView/WriteReview.js
+++ src/views/reports/InterviewerDashboardView/WriteReview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -6,8 +6,19 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import { Card, CardContent, Divider, TextField} from '@material-ui/core';
+import swal from 'sweetalert';
+import InterviewerService from '../../../services/InterviewerService';
+
+import {
+  Card,
+  CardContent,
+  Divider,
+  TextField,
+  Button
+} from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
+import axios from 'axios';
+import Auth from '../../../auth';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -32,111 +43,205 @@ function TabPanel(props) {
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
 };
 
 function a11yProps(index) {
   return {
     id: `scrollable-auto-tab-${index}`,
-    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`
   };
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
+    backgroundColor: theme.palette.background.paper
+  }
 }));
 
-export default function ScrollableTabsButtonAuto() {
+export default function ReviewTabs(props) {
+  const { rounds, drive_id, student_id } = props;
+
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
+  const [technical, setTechnical] = React.useState([]);
+  const [communication, setCommunication] = React.useState([]);
+  const [personality, setPersonality] = React.useState([]);
+  const [comment, setComment] = React.useState([]);
+  const [reviewPresent, setReviewPresent] = React.useState([]);
+  const [reviewReceived, setReviewReceived] = React.useState(false);
+
+  const handleTabChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
-          <Tab label="Item Six" {...a11yProps(5)} />
-          <Tab label="Item Seven" {...a11yProps(6)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-      <Typography className={classes.bold} variant="h6">
-                    Technical
-                  </Typography>
-                    <Rating
-                      name="technical"
-                      value={technical}
-                      onChange={(event, newValue) => {
-                        setTechnical(newValue);
-                      }}
-                    />
-                  <Typography className={classes.bold} variant="h6">
-                  Communication
-                      </Typography>
-                    <Rating
-                      name="communication"
-                      value={communication}
-                      onChange={(event, newValue) => {
-                        setCommunication(newValue);
-                      }}
-                    />
-                  <Typography className={classes.bold} variant="h6">
-                    Personality
-                  </Typography>
-                    <Rating
-                      name="personality"
-                      value={personality}
-                      onChange={(event, newValue) => {
-                        setPersonality(newValue);
-                      }}
-                    />
-                  <br/>
-                  <TextField
-                    id="comment"
-                    label="Write a Review"
-                    multiline
-                    rows={4}
-                    variant="outlined"
-                  />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
-    </div>
-  );
+  const handleSubmit = (e, round_number) => {
+    e.preventDefault();
+    const data = {
+      student: student_id,
+      driveList: [
+        {
+          drive: drive_id,
+          reviewData: [
+            {
+              round_number: round_number,
+              interviewer: 1,
+              technical: technical[round_number],
+              communication: communication[round_number],
+              personality: personality[round_number],
+              comment: comment[round_number]
+            }
+          ]
+        }
+      ]
+    };
+
+    console.log(data);
+    axios
+      .post('http://20.37.50.140:8000/api/review/', data, {
+        headers: {
+          'Content-type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest',
+          Authorization: 'Token ' + Auth.getToken()
+        }
+      })
+      .then(res => {
+        console.log('res', res);
+        swal('Review Submitted Successsfully', 'Thank You!', 'success');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    InterviewerService.getStudentReviews(student_id)
+      .then(res => {
+        let tech = [...technical];
+        let commu = [...communication];
+        let pers = [...personality];
+        let comm = [...comment];
+        let rev = [...reviewPresent];
+        console.log('Student Review--- ', res.data);
+        res.data.reviewData.forEach(review => {
+          tech[review.round_number] = review.technical;
+          commu[review.round_number] = review.communication;
+          pers[review.round_number] = review.personality;
+          comm[review.round_number] = review.comment;
+          rev[review.round_number] = true;
+        });
+        setTechnical([...tech]);
+        setCommunication([...commu]);
+        setPersonality([...pers]);
+        setComment([...comm]);
+        setReviewReceived([...rev]);      
+        // setDrive(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {}, [reviewReceived]);
+
+  if (reviewReceived) {
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleTabChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+            aria-label="scrollable auto tabs example"
+          >
+            {rounds.map((round, index) => (
+              <Tab label={round.name} {...a11yProps(index)} />
+            ))}
+          </Tabs>
+        </AppBar>
+        {rounds.map((round, index) => (
+          <TabPanel value={value} index={index}>
+            <form
+              onSubmit={e => handleSubmit(e, round.number)}
+              autoComplete="off"
+              noValidate
+            >
+              <Typography className={classes.bold} variant="h6">
+                Technical
+              </Typography>
+              <Rating
+                name="technical"
+                value={technical[round.number]}
+                readOnly={reviewReceived[round.number]}
+                onChange={(event, newValue) => {
+                  let tech = [...technical];
+                  tech[round.number] = newValue;
+                  setTechnical([...tech]);
+                }}
+              />
+              <Typography className={classes.bold} variant="h6">
+                Communication
+              </Typography>
+              <Rating
+                name="communication"
+                value={communication[round.number]}
+                readOnly={reviewReceived[round.number]}
+                onChange={(event, newValue) => {
+                  let commu = [...communication];
+                  commu[round.number] = newValue;
+                  setCommunication([...commu]);
+                }}
+              />
+              <Typography className={classes.bold} variant="h6">
+                Personality
+              </Typography>
+              <Rating
+                name="personality"
+                value={personality[round.number]}
+                readOnly={reviewReceived[round.number]}
+                onChange={(event, newValue) => {
+                  let perso = [...personality];
+                  perso[round.number] = newValue;
+                  setPersonality([...perso]);
+                  console.log(personality);
+                }}
+              />
+              <br />
+              <TextField
+                id="comment"
+                label="Review"
+                disabled={reviewReceived[round.number]}
+                multiline
+                onChange={(event, newValue) => {
+                  let comm = [...comment];
+                  comm[round.number] = event.target.value;
+                  setComment([...comm]);
+                  console.log(comment);
+                }}
+                rows={4}
+                value={comment[round.number]}
+                variant="outlined"
+              />
+            { !reviewReceived[round.number] &&
+              <Box display="flex" p={2}>
+                <Button color="primary" variant="contained" type="submit">
+                  Submit
+                </Button>
+              </Box>
+            }
+            </form>
+          </TabPanel>
+        ))}
+      </div>
+    );
+  }
+  else{
+    return null;
+  }
 }
