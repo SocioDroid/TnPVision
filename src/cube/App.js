3431bb72557f49858198c1ac743305783135ea6b
leet <sociodroid2019@gmail.com>
2021-03-02 09:44:46 +0530

    Cubejs

---
+++ src/cube/App.js
@@ -0,0 +1,41 @@
+import './body.css';
+import 'antd/dist/antd.css';
+import React from 'react';
+import '@ant-design/compatible';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Layout } from 'antd';
+import cubejs from '@cubejs-client/core';
+import { CubeProvider } from '@cubejs-client/react';
+import client from './graphql/client';
+import Header from './components/Header';
+import ExplorePage from './pages/ExplorePage';
+import DashboardPage from './pages/DashboardPage';
+const API_URL = 'http://localhost:4000';
+const CUBEJS_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTQ1OTM2MDIsImV4cCI6MTYxNDY4MDAwMn0.rZXcgYT_VCnxiUAuo7f0xdV10jSHqztpEPP5XibnoqI';
+const cubejsApi = cubejs(CUBEJS_TOKEN, {
+  apiUrl: `${API_URL}/cubejs-api/v1`
+});
+
+const AppLayout = ({ children }) => (
+  <Layout
+    style={{
+      height: '100%'
+    }}
+  >
+    {/* <Header /> */}
+    <Layout.Content>
+      {children}
+    </Layout.Content>
+  </Layout>
+);
+
+const App = ({ children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <ApolloProvider client={client}>
+      <AppLayout>{children}</AppLayout>
+    </ApolloProvider>
+  </CubeProvider>
+);
+
+export default App;
